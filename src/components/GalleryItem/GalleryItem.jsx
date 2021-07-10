1f02de7e19fc1559a07b54477ac5a00b788ae9b0
Steve Savstrom <steve.savstrom@gmail.com>
2021-07-10 11:35:03 -0500

    toggle description working

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,20 +1,31 @@
 import "./GalleryItem.css";
-import { useState } from 'react';
+import { useState } from "react";
 
 function GalleryItem({ gallery }) {
-  const [ count, setCount ] = useState(0);
+  const [count, setCount] = useState(0);
+  const [image, setImage] = useState(true);
 
   const increaseLikes = () => {
-    setCount(count +1)
-  }
+    setCount(count + 1);
+  };
+
+  const toggleText = () => {
+    setImage(!image);
+  };
 
   return (
     <>
       <section id="item">
-        <img src={gallery.url} width="150"></img>
+        <div class="container">
+          <button id="imageButton" onClick={toggleText}>
+            <img src={gallery.url} width="150"></img>
+            <div class="centered">{!image && <p>{gallery.description}</p>}</div>
+          </button>
+        </div>
+
         <p>{gallery.description}</p>
-        <button onClick ={increaseLikes}>Likes</button>
-        <div>Likes: {count}</div>
+        <button onClick={increaseLikes}>❤️ {count}</button>
+        {/* <div>Likes: {count}</div> */}
       </section>
     </>
   );
