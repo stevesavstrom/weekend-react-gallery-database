8f5e062c94d9cf91650e3d6552cfa829ca8b7166
Steve Savstrom <steve.savstrom@gmail.com>
2021-07-10 10:46:24 -0500

    like counter working on DOM - not connected to database

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,12 +1,20 @@
 import "./GalleryItem.css";
+import { useState } from 'react';
 
 function GalleryItem({ gallery }) {
+  const [ count, setCount ] = useState(0);
+
+  const increaseLikes = () => {
+    setCount(count +1)
+  }
+
   return (
     <>
       <section id="item">
         <img src={gallery.url} width="150"></img>
         <p>{gallery.description}</p>
-        <button>Like!</button>
+        <button onClick ={increaseLikes}>Likes</button>
+        <div>Likes: {count}</div>
       </section>
     </>
   );
