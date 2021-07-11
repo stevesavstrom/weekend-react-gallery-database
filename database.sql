b23cdeb5005804f8646b320cf750f2fd02f11032
Steve Savstrom <steve.savstrom@gmail.com>
2021-07-11 09:25:29 -0500

    added database.sql with directions for setting up database

---
+++ database.sql
@@ -0,0 +1,19 @@
+-- Database setup
+
+-- Create a database called `react_gallery`
+
+CREATE TABLE gallery (
+	id SERIAL PRIMARY KEY,
+	url VARCHAR (500),
+	description varchar(500),
+	likes integer DEFAULT 0
+);
+
+INSERT INTO gallery (url, description)
+VALUES ('https://raw.githubusercontent.com/stevesavstrom/weekend-react-gallery-database/master/public/images/photo1.png', 'Hiking in Sedona, Arizona'),
+('https://raw.githubusercontent.com/stevesavstrom/weekend-react-gallery-database/master/public/images/photo9.png', 'Biking in Chicago'),
+('https://raw.githubusercontent.com/stevesavstrom/weekend-react-gallery-database/master/public/images/photo5.png', 'Hanging out in Waterton Lakes National Park'),
+('https://raw.githubusercontent.com/stevesavstrom/weekend-react-gallery-database/master/public/images/photo2.png', 'My wfe, Colleen and I in Grand Marais, MN'),
+('https://raw.githubusercontent.com/stevesavstrom/weekend-react-gallery-database/master/public/images/photo7.png', 'Biking in Minneapolis'),
+('https://raw.githubusercontent.com/stevesavstrom/weekend-react-gallery-database/master/public/images/photo4.png', 'Honeymoon in Banff'),
+('https://raw.githubusercontent.com/stevesavstrom/weekend-react-gallery-database/master/public/images/photo3.png', 'Skiing on the North Shore');
