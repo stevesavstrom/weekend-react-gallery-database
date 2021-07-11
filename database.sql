667fededfd5f249eac20db2ef353f4e0954e24e5
Steve Savstrom <steve.savstrom@gmail.com>
2021-07-11 10:56:02 -0500

    updating database and database.sql directions

--- database.sql
+++ database.sql
@@ -12,8 +12,12 @@ CREATE TABLE gallery (
 INSERT INTO gallery (url, description)
 VALUES ('https://raw.githubusercontent.com/stevesavstrom/weekend-react-gallery-database/master/public/images/photo1.png', 'Hiking in Sedona, Arizona'),
 ('https://raw.githubusercontent.com/stevesavstrom/weekend-react-gallery-database/master/public/images/photo9.png', 'Biking in Chicago'),
+('https://raw.githubusercontent.com/stevesavstrom/weekend-react-gallery-database/master/public/images/photo10.png', 'Wedding day in 2017!'),
 ('https://raw.githubusercontent.com/stevesavstrom/weekend-react-gallery-database/master/public/images/photo5.png', 'Hanging out in Waterton Lakes National Park'),
 ('https://raw.githubusercontent.com/stevesavstrom/weekend-react-gallery-database/master/public/images/photo2.png', 'My wfe, Colleen and I in Grand Marais, MN'),
 ('https://raw.githubusercontent.com/stevesavstrom/weekend-react-gallery-database/master/public/images/photo7.png', 'Biking in Minneapolis'),
 ('https://raw.githubusercontent.com/stevesavstrom/weekend-react-gallery-database/master/public/images/photo4.png', 'Honeymoon in Banff'),
+('https://raw.githubusercontent.com/stevesavstrom/weekend-react-gallery-database/master/public/images/photo6.png', 'Teaching in Chicago'),
+('https://raw.githubusercontent.com/stevesavstrom/weekend-react-gallery-database/master/public/images/photo8.png', 'Teaching in Minneapolis'),
 ('https://raw.githubusercontent.com/stevesavstrom/weekend-react-gallery-database/master/public/images/photo3.png', 'Skiing on the North Shore');
+
